3c2f06b73ef7e93bcfcbf87ce8453f4c6facbd4f
Catalin Chiorean <cata.al.ch@gmail.com>
2022-07-14 21:50:23 +0300

    Added hooks test

---
+++ tests/hooksTest.js
@@ -0,0 +1,25 @@
+fixture("First Fixture")
+    .page("http://devexpress.github.io/testcafe/")
+    .beforeEach(async t => {
+        await t
+            .setTestSpeed(0.1)
+            .setPageLoadTimeout(0);
+    });
+
+test
+    .page("https://devexpress.github.io/testcafe/example/")
+    ("First Test", async t => {
+        await t
+            .typeText("#developer-name", "Cata")
+            .click("#macos")
+            .click("#submit-button");
+    });
+
+test
+    .page("https://devexpress.github.io/testcafe/example/")
+    ("Second Test", async t => {
+        await t
+            .typeText("#developer-name", "Cata")
+            .click("#macos")
+            .click("#submit-button");
+    });
